2497cc6eef88c46a83e90fab48c727527f3643bb
zggsong <zggsong@foxmail.com>
2024-11-27 11:16:45 +0800

    perf: 使用net472

--- src/LangIndicator/MainWindow.xaml.cs
+++ src/LangIndicator/MainWindow.xaml.cs
@@ -1,5 +1,4 @@
-﻿using System.Diagnostics.CodeAnalysis;
-using System.Runtime.InteropServices;
+﻿using System.Runtime.InteropServices;
 using System.Windows;
 using System.Windows.Media;
 using System.Windows.Media.Animation;
